fed1cf234619ea4603bc4601d867812e86de0985
Moiz Ali Afzal <i202334@nu.edu.pk>
2024-03-30 16:16:41 +0500

    making final change to app.py

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ y = df['next_close']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = \
-train_test_split(X, y, test_size=0.2, 
-random_state=42)
+    train_test_split(X, y, test_size=0.2,
+                     random_state=42)
 
 # Initialize and train the model
 model = RandomForestRegressor(n_estimators=100, random_state=42)
