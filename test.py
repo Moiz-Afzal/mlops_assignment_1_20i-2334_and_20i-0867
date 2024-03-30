7d87981217050b56e9e917aafb1f401cbcc2063f
Abdullah5844 <abdullahinam666@gmail.com>
2024-03-30 22:42:07 +0500

    feat: Added more Test cases for Testing Automation

--- test.py
+++ test.py
@@ -17,5 +17,17 @@ class TestConvertTimestampToDatetime(unittest.TestCase):
         self.assertEqual(actual_datetime_str, expected_datetime)
 
 
+    def test_convert_timestamp_to_datetime1(self):
+        # Test with a sample timestamp in milliseconds
+        timestamp_ms = 1617195600002
+        expected_datetime = "2021-03-31 18:01:00"
+        actual_datetime = convert_timestamp_to_datetime(timestamp_ms)
+
+        # Convert actual datetime to string for comparison
+        actual_datetime_str = actual_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+        # Check
+        self.assertEqual(actual_datetime_str, expected_datetime)
+
 if __name__ == '__main__':
     unittest.main()
