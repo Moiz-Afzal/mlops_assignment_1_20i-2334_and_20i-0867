4fba8acb9d72741c40e7640c7f2c029f74ee8bb1
Moiz Ali Afzal <i202334@nu.edu.pk>
2024-03-30 15:42:03 +0500

    pushing to dev with flask8

---
+++ app.py
@@ -0,0 +1,66 @@
+import requests
+import pandas as pd
+import datetime
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+def convert_timestamp_to_datetime(timestamp_ms):
+    timestamp_seconds = timestamp_ms / 1000
+    return datetime.datetime.fromtimestamp(timestamp_seconds)
+
+# Define the API URL
+url = "https://api.polygon.io/v2/aggs/ticker/C:EURUSD/range/15/minute/2020-01-09/2023-01-20?adjusted=true&sort=asc&limit=1200000&apiKey=USGhqM149TAOaKXNbLratZdnuuTmJL9k"
+
+# Fetch data from the API
+response = requests.get(url)
+data = response.json()
+
+# Extract the 'results' list from the JSON
+results = data['results']
+
+# Convert the 'results' list into a DataFrame
+df = pd.DataFrame(results)
+
+# Apply the timestamp conversion function to the 't' column
+df['t'] = df['t'].apply(convert_timestamp_to_datetime)
+
+#v: Volume at any given point
+#vw: Volume Weighted Average Price (VWAP)
+#o: Opening Price
+#c: Closing Price
+#h: Highest Price (High)
+#l: Lowest Price (Low)
+#t: Timestamp (in milliseconds since Unix epoch) converted to day time
+#n: Number of items in this aggregation interval / number of stock points responsible for finding the OCVHL Value
+
+
+# Drop irrelevant columns
+df.drop(columns=['n'], inplace=True)
+
+# Shift the 'c' column to create the target variable
+df['next_close'] = df['c'].shift(-1)
+
+# Drop rows with NaN (last row where 'next_close' is NaN)
+df.dropna(inplace=True)
+
+# Define features and target variable
+X = df.drop(columns=['next_close', 't'])
+y = df['next_close']
+
+# Split the data into training and testing sets (80% train, 20% test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Initialize and train the model
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Predict the test set
+y_pred = model.predict(X_test)
+
+# Calculate Mean Squared Error
+mse = mean_squared_error(y_test, y_pred)
+# print("Y predict",y_pred)
+print("Mean Squared Error:", mse)
